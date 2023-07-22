Fix shape normalization for scalar tensors.

mininf/core.py
@@ -314,7 +314,7 @@ def sample(state: State, name: str, distribution: Distribution, sample_shape: Op
             >>> from mininf import sample
             >>> from torch.distributions import Normal
 
-            >>> sample("x", Normal(0, 1), (3,))
+            >>> sample("x", Normal(0, 1), 3)
             tensor([..., ..., ...])
     """
     tracer = TracerMixin.get_instance()

tests/test_mininf.py
@@ -6,7 +6,7 @@ def test_linear_regression_forward_backward() -> None:
     # Declare the model.
     def linear_regression(n: int, p: int) -> None:
         features = mininf.sample("features", torch.distributions.Normal(0, 1), (n, p))
-        coefs = mininf.sample("coefs", torch.distributions.Normal(0, 1), (p,))
+        coefs = mininf.sample("coefs", torch.distributions.Normal(0, 1), p)
         sigma = mininf.sample("sigma", torch.distributions.Gamma(2, 2))
         predictions = features @ coefs
         mininf.sample("outcomes", torch.distributions.Normal(predictions, sigma))

tests/test_nn.py
@@ -49,7 +49,7 @@ def test_factorized_distribution() -> None:
 
 def test_evidence_lower_bound_loss_with_grad() -> None:
     def model() -> None:
-        mininf.sample("x", torch.distributions.Normal(0, 1), (3,))
+        mininf.sample("x", torch.distributions.Normal(0, 1), 3)
 
     approximation = ParameterizedDistribution(torch.distributions.Normal, loc=0.0,
                                               scale=torch.ones(3))
@@ -113,7 +113,7 @@ def test_factorized_parameterized_distribution() -> None:
 
 def test_log_likelihood_loss_with_grad() -> None:
     def model() -> None:
-        mininf.sample("x", torch.distributions.Normal(0, 1), (3,))
+        mininf.sample("x", torch.distributions.Normal(0, 1), 3)
 
     estimate = torch.nn.Parameter(torch.ones(3))
     loss = LogLikelihoodLoss()
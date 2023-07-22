f6f9299a2bded713ad120374dc4d18e22152534c
Till Hoffmann <tillahoffmann@gmail.com>
2023-07-22 11:58:18 -0400

    Add targets and workflow steps to publish to PyPI.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import find_packages, setup
 
+description = "mininf is a minimal library to infer the parameters of probabilistic programs and " \
+    "make predictions"
+long_description = f"{description}. See [the documentation](https://mininf.readthedocs.io) for " \
+    "details."
+
 setup(
     name="mininf",
     version="0.1",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "torch",
